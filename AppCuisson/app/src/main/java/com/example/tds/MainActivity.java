e8cd470a1cb3682c2d84d061c6ac4a27ff1df51a
Morgan Nayet <Morgan-Nayet@users.noreply.github.com>
2022-02-23 14:13:50 +0100

    fix #3 Lancement page Afficher

--- AppCuisson/app/src/main/java/com/example/tds/MainActivity.java
+++ AppCuisson/app/src/main/java/com/example/tds/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.tds;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-
-import com.example.appcuisson.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,46 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mp.start();
 
         // variables d'interraction UI
-        budgetTransport = findViewById(R.id.saisieTransport);
-        budgetNuit = findViewById(R.id.saisieNuit);
-        budgetRepas = findViewById(R.id.saisieRepas);
-        budgetVisiteSortie = findViewById(R.id.saisieVisiteSortie);
-        cout = findViewById(R.id.affichCout);
-        nbJours = findViewById(R.id.saisieNbJours);
-        repas2DernierJour = findViewById(R.id.saisieRepas2);
-        Button boutonCalculer = findViewById(R.id.calculer);
-        Button boutonAnnuler = findViewById(R.id.annuler);
 
         //on associe un écouteur à chaque bouton
-        boutonCalculer.setOnClickListener(this);
-        boutonAnnuler.setOnClickListener(this);
 
         //on enlève le cache sur les EditText de type numberPassword
-        budgetTransport.setTransformationMethod(null);
-        budgetNuit.setTransformationMethod(null);
-        budgetRepas.setTransformationMethod(null);
-        budgetVisiteSortie.setTransformationMethod(null);
     }
 
     public void onClick(View view){
 
-        if(view.getId() == R.id.calculer ){
-            if(     budgetTransport.getText().toString().trim().equals("")
-                    ||budgetNuit.getText().toString().trim().equals("")
-                    ||budgetRepas.getText().toString().trim().equals("")
-                    ||budgetVisiteSortie.getText().toString().trim().equals("")
-                    || (Integer.parseInt(String.valueOf(budgetTransport.getText())) >= 0
-                    && Integer.parseInt(String.valueOf(budgetNuit.getText())) >= 0
-                    && Integer.parseInt(String.valueOf(budgetRepas.getText())) >= 0
-                    && Integer.parseInt(String.valueOf(budgetVisiteSortie.getText())) >= 0)
-            ) {
-                calculerReduc();
-            }
-        }
-
-        if(view.getId() == R.id.annuler ){
-            initAppli();
-        }
     }
 
     /**
@@ -98,67 +62,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mp.start();
     }
 
-
-    /**
-     * Affiche les résultats du calcul
-     */
-    @SuppressLint("SetTextI18n")
-    public void calculerReduc(){
-
-        int pxTransport, pxNuit, pxRepas, pxVisiteSortie, nbNuits, dernierRepas, tarifFinal;
-
-        //calcul des différents éléments
-        if( budgetTransport.getText().toString().trim().equals("") ) {
-            pxTransport = 0;
-        } else {
-            pxTransport = Integer.parseInt(String.valueOf(budgetTransport.getText()));
-        }
-        if( budgetNuit.getText().toString().trim().equals("") ) {
-            pxNuit = 0;
-        } else {
-            pxNuit = Integer.parseInt(String.valueOf(budgetNuit.getText()));
-        }
-        if( budgetRepas.getText().toString().trim().equals("") ) {
-            pxRepas = 0;
-        } else {
-            pxRepas = Integer.parseInt(String.valueOf(budgetRepas.getText()));
-        }
-        if( budgetVisiteSortie.getText().toString().trim().equals("") ) {
-            pxVisiteSortie = 0;
-        } else {
-            pxVisiteSortie = Integer.parseInt(String.valueOf(budgetVisiteSortie.getText()));
-        }
-        if( (nbNuits = nbJours.getCheckedRadioButtonId()) == R.id.jours_2 ) {
-            nbNuits = 1;
-        } else if(nbNuits == R.id.jours_3) {
-            nbNuits = 2;
-        } else if(nbNuits == R.id.jours_4) {
-            nbNuits = 3;
-        } else {
-            nbNuits = 4;
-        }
-
-        dernierRepas = repas2DernierJour.isChecked() ? 1 : 0;
-
-        //calcul du tarif final
-        tarifFinal = (nbNuits*2+1+dernierRepas)*pxRepas + (nbNuits+1)*pxVisiteSortie + nbNuits * pxNuit + pxTransport;
-
-        cout.setText(tarifFinal + " " + getString(R.string.euros));
-
-    }
-
     /**
      * Affiche l'état de l'application par défaut
      */
     public void initAppli(){
 
-        budgetTransport.setText("");
-        budgetNuit.setText("");
-        budgetRepas.setText("");
-        budgetVisiteSortie.setText("");
-        cout.setText("");
-        nbJours.clearCheck();
-        nbJours.check(R.id.jours_2);
-        repas2DernierJour.setChecked(false);
     }
 }
